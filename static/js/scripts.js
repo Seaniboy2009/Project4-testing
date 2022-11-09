66c2c38b0c44652b5b3fd3d29db2a53494af1bf9
Sean Clark <seaniboy2009@yahoo.co.uk>
2022-11-09 16:04:01 +0000

    updated index.html with user reviews

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOM loaded");
     let menu_button = document.getElementById("menu-toggle");
-    let menu_list = document.getElementById("navbarSupportedContent")
+    let menu_list = document.getElementById("navbarSupportedContent");
+    let copyright_text = document.getElementById("copyright");
 
+    // Menu button toggle
     menu_button.addEventListener("click", () => {
         console.log("menu button clicked")
         menu_list.classList.toggle("collapse");
     });
+
+    // update copyright date
+    copyright_text.innerText = new Date().getFullYear();
 })
