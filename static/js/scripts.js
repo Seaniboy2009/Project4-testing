3bcb056e1bb6c1e9071aa18bf7151b957d9b24be
Sean Clark <seaniboy2009@yahoo.co.uk>
2022-11-09 14:01:32 +0000

    added function to hide/unhide nav menu

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOM loaded");
     let menu_button = document.getElementById("menu-toggle");
+    let menu_list = document.getElementById("navbarSupportedContent")
 
     menu_button.addEventListener("click", () => {
         console.log("menu button clicked")
+        menu_list.classList.toggle("collapse");
+
     });
 })
