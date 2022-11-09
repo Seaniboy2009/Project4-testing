c617c6029b72dfd26a35779fb987d903147670af
Sean Clark <seaniboy2009@yahoo.co.uk>
2022-11-09 13:39:47 +0000

    added background image, linked script.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,10 +1,8 @@
-/*!
-* Start Bootstrap - Business Casual v7.0.8 (https://startbootstrap.com/theme/business-casual)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
-*/
-// Highlights current date on contact page
-window.addEventListener('DOMContentLoaded', event => {
-    const listHoursArray = document.body.querySelectorAll('.list-hours li');
-    listHoursArray[new Date().getDay()].classList.add(('today'));
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM loaded");
+    let menu_button = document.getElementById("menu-toggle");
+
+    menu_button.addEventListener("click", () => {
+        console.log("menu button clicked")
+    });
 })
